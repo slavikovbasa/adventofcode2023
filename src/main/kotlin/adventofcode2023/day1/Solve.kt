27ea0fc619f513ba723090269726d2f3b230eb69
s.kovbasa <s.kovbasa@smartweb.com.ua>
2023-12-10 06:40:42 +0200

    Day1 simplify code

--- src/main/kotlin/adventofcode2023/day1/Solve.kt
+++ src/main/kotlin/adventofcode2023/day1/Solve.kt
@@ -2,11 +2,11 @@ package adventofcode2023.day1
 
 object Solve {
     const val URL = "https://adventofcode.com/2023/day/1/input"
-    
+
     fun first(text: String): Int {
         return text.trim().lines().sumOf { digitsFromLineSimple(it) }
     }
-    
+
     fun second(text: String): Int {
         return text.trim().lines().sumOf { digitsFromLineComplex(it) }
     }
@@ -19,36 +19,37 @@ fun digitsFromLineSimple(line: String): Int {
 
 
 val numbers = mapOf(
-    "one" to 1,
-    "two" to 2,
-    "three" to 3,
-    "four" to 4,
-    "five" to 5,
-    "six" to 6,
-    "seven" to 7,
-    "eight" to 8,
-    "nine" to 9,
-    "zero" to 0,
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+        "zero" to 0,
 )
+
+
+fun getFirstDigit(line: String): Int? = if (line[0].isDigit()) {
+    line[0].digitToInt()
+} else {
+    numbers.firstNotNullOfOrNull {
+        if (line.startsWith(it.key)) {
+            it.value
+        } else {
+            null
+        }
+    }
+}
 
 fun digitsFromLineComplex(line: String): Int {
     var first: Int? = null;
     var last: Int? = null;
     for (i in line.indices) {
-        if (line[i].isDigit()) {
-            last = line[i].digitToInt()
-        } else {
-            val slice = line.substring(i)
-            val strNum = numbers.firstNotNullOfOrNull {
-                if (slice.startsWith(it.key)) { it.value } else { null }
-            }
-            if (strNum != null) {
-                last = strNum
-            }
-        }
-        if (first == null && last != null) {
-            first = last
-        }
+        last = getFirstDigit(line.substring(i)) ?: continue
+        first = first ?: last
     }
 
     return "${first}${last}".toInt()
